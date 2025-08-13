4712a8e207de028bd9da0c4e92bf799fbb0cc137
kim henry <kimanihenry14489@gmail.com>
2025-08-13 22:03:54 +0300

    Finished setting up electron

---
+++ src/electron/main.js
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron/main';
+import path from 'node:path';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
